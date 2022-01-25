a6e74ec7d0b3f609337785a78d1e46d1092d77e1
Borjaarmet <borjaarmet@gmail.com>
2022-01-25 14:03:12 +0100

    edit item

--- src/App.js
+++ src/App.js
@@ -20,6 +20,18 @@ function App() {
       showAlert(true, "danger", "please enter value");
     } else if (name && isEditing) {
       // deal with edit
+      setList(
+        list.map((item) => {
+          if (item.id == editID) {
+            return { ...item, title: name };
+          }
+          return item;
+        })
+      );
+      setName("");
+      setIsEditing(false);
+      setEditID(null);
+      setAlert(true, "success", "value changed");
     } else {
       const newItem = { id: new Date().getTime().toString(), title: name };
       setList([...list, newItem]);
@@ -40,6 +52,12 @@ function App() {
   const removeItem = (id) => {
     showAlert(true, "success", "removed item");
     setList(list.filter((item) => item.id !== id));
+  };
+  const editItem = (id) => {
+    const specificItem = list.find((item) => item.id == id);
+    setIsEditing(true);
+    setEditID(id);
+    setName(specificItem.title);
   };
   return (
     <section className="section-center">
@@ -68,7 +86,7 @@ function App() {
         </div>
       ) : (
         <div className="grocery-container">
-          <List items={list} removeItem={removeItem} />
+          <List items={list} removeItem={removeItem} editItem={editItem} />
           <button className="clear-btn" onClick={clearList}>
             Clear items
           </button>
